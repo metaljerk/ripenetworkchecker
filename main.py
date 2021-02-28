b1f4b8f6ede307a3efdc13639a0ed717b279ceff
MetalJerk <scottlonghofer@gmail.com>
2021-02-28 14:57:05 -0500

    rearrange

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ parser.add_argument('--ip', metavar='ip', type=str, required=True,
                     help="Checks the IP in a decimal format. e.g. 192.168.1.1")
 args = parser.parse_args()
  
-url = "https://stat.ripe.net/data/country-resource-list/data.json?resource=US&v4_format=prefix"
-response = requests.get(url)
-data = response.json()
 def iplist(url):
+    url = "https://stat.ripe.net/data/country-resource-list/data.json?resource=US&v4_format=prefix"
+    response = requests.get(url)
+    data = response.json()
     for ipv4 in data['data']['resources']['ipv4']:
         yield ipv4
 
